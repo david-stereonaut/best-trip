82d8b61e222062d0f67117329dc0cf5eb015c11c
david.stereonaut <adonbarnash@gmail.com>
2020-12-08 11:17:51 +0200

    setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const databaseApi = require('./server/routes/databaseApi')
+const externalApi = require('./server/routes/externalApi')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use('/', databaseApi)
+app.use('/', externalApi)
+
+const port = 3000
+app.listen((process.env.PORT || port), function() {
+    console.log(`Server running on port ${port}`)
+})
