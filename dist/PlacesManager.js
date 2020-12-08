fb9025f136a80718486129bf44ade9a5b920912d
david.stereonaut <adonbarnash@gmail.com>
2020-12-08 12:30:27 +0200

    places manager done (no pagination options)

--- dist/PlacesManager.js
+++ dist/PlacesManager.js
@@ -9,7 +9,15 @@ class PlacesManager {
         this.currentResults = {}
     }
 
-    async getPlaces(placeName, category) {
-        
+    async getPlaces(searchString, category) {
+        let places = await $.get(`/places/${searchString}/${category}`)
+        this.results = places.results
+        return this.results
+    }
+
+    async getPlace(place_id) {
+        let placeDetails = await $.get(`/place/${place_id}`)
+        this.currentResults = placeDetails
+        return this.currentResults
     }
 }
