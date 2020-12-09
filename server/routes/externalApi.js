3c34f5bf1d0741e4afa910f3908837aead10aa97
david.stereonaut <adonbarnash@gmail.com>
2020-12-09 22:25:36 +0200

    fixed stuff

--- server/routes/externalApi.js
+++ server/routes/externalApi.js
@@ -9,39 +9,44 @@ const GOOGLE_API = process.env.GOOGLE_API
 
 router.post('/flights', async function (req, res) {
     const flight = req.body
-    let airportFrom = await axios.get(`https://www.air-port-codes.com/api/v1/multi?type=a&term=${flight.flyFrom}`, {
-        headers: {
-            'APC-Auth': '6f4fb09668', 'APC-Auth-Secret': 'fb132f8cb880647'
-        }
-    })
-    airportFrom = airportFrom.data.airports[0].iata || airportFrom.data.airports.iata
+    try {
 
-    let airportTo = await axios.get(`https://www.air-port-codes.com/api/v1/multi?type=a&term=${flight.flyTo}`, {
-        headers: {
-            'APC-Auth': '6f4fb09668', 'APC-Auth-Secret': 'fb132f8cb880647'
-        }
-    })
-    airportTo = airportTo.data.airports[0].iata || airportFrom.data.airports.iata
-
-    let flightsData = await axios.get(`https://api.skypicker.com/flights?flyFrom=${airportFrom}&to=${airportTo}&dateFrom=${flight.dateStart}&dateTo=${flight.dateEnd}&partner=picky&v=3`)
-    let fly = flightsData.data.data
-    let flightsArray = fly.map(f =>
-        ({
-            dtime: f.dTime,
-            aTime: f.aTime,
-            cityFrom: f.cityFrom,
-            airportFromCod: f.flyFrom,
-            countryFrom: f.countryFrom.name,
-            cityTo: f.cityTo,
-            airportToCod: f.flyTo,
-            countryTo: f.countryTo.name,
-            price: f.conversion.EUR,
-            availability: f.availability.seats,
-            link: f.deep_link,
-            transfers: f.transfers,
-            fly_duration: f.fly_duration
-        }))
-    res.send(flightsArray)
+        let airportFrom = await axios.get(`https://www.air-port-codes.com/api/v1/multi?type=a&term=${flight.flyFrom}`, {
+            headers: {
+                'APC-Auth': '6f4fb09668', 'APC-Auth-Secret': 'fb132f8cb880647'
+            }
+        })
+        airportFrom = airportFrom.data.airports[0].iata || airportFrom.data.airports.iata
+    
+        let airportTo = await axios.get(`https://www.air-port-codes.com/api/v1/multi?type=a&term=${flight.flyTo}`, {
+            headers: {
+                'APC-Auth': '6f4fb09668', 'APC-Auth-Secret': 'fb132f8cb880647'
+            }
+        })
+        airportTo = airportTo.data.airports[0].iata || airportFrom.data.airports.iata
+    
+        let flightsData = await axios.get(`https://api.skypicker.com/flights?flyFrom=${airportFrom}&to=${airportTo}&dateFrom=${flight.dateStart}&dateTo=${flight.dateEnd}&partner=picky&v=3`)
+        let fly = flightsData.data.data
+        let flightsArray = fly.map(f =>
+            ({
+                dTime: f.dTime,
+                aTime: f.aTime,
+                cityFrom: f.cityFrom,
+                airportFromCod: f.flyFrom,
+                countryFrom: f.countryFrom.name,
+                cityTo: f.cityTo,
+                airportToCod: f.flyTo,
+                countryTo: f.countryTo.name,
+                price: f.conversion.EUR,
+                availability: f.availability.seats,
+                link: f.deep_link,
+                transfers: f.transfers,
+                fly_duration: f.fly_duration
+            }))
+        res.send(flightsArray)
+    } catch {
+        res.send({ error: 'no flights found' })
+    }
 })
 
 // router.get('/placeLocation/:place', function (req, res) {
@@ -58,7 +63,7 @@ router.get('/places/:city/:category', async function (req, res) {
     const category = req.params.category
     const makeCitytoLatandLong = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${cityName}&key=AIzaSyBZbfnMyK4xaIDNevsXwulDnxC9nhZ0rS0`)
     let location = makeCitytoLatandLong.data.results[0].geometry.location
-    const getPlaces = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=10000&&language=entype=${category}&key=AIzaSyBZbfnMyK4xaIDNevsXwulDnxC9nhZ0rS0`)
+    const getPlaces = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.lat},${location.lng}&radius=10000&language=en&type=${category}&key=AIzaSyBZbfnMyK4xaIDNevsXwulDnxC9nhZ0rS0`)
     const results = getPlaces.data.results
     const places = results.map(r => ({
         name: r.name,
